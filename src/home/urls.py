c07afb87dc754652f4c59688f335e8760d211d88
Katarzyna Kowalska <k.a.kowalska@outlook.com>
2025-02-10 20:12:57 +0700

    HTML login form and template

--- src/home/urls.py
+++ src/home/urls.py
@@ -2,9 +2,12 @@ from django.contrib import admin
 from django.urls import path
 
 from .views import home_view, about_page
+from auth import views as auth_views
 
 urlpatterns = [
+    path("", home_view),
+    path("login/", auth_views.login_view),
     path('admin/', admin.site.urls),
-    path("home/", home_view),
     path("about/", about_page),
+
 ]

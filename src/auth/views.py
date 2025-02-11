1ea38829c7ce332353e06b0c98d2343efe0ac912
Katarzyna Kowalska <k.a.kowalska@outlook.com>
2025-02-11 16:24:25 +0700

    added register route

--- src/auth/views.py
+++ src/auth/views.py
@@ -1,7 +1,10 @@
 from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
 
-# Create your views here.
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
 
 
 def login_view(request):
@@ -17,5 +20,19 @@ def login_view(request):
     return render(request, 'auth/login.html')
 
 
-# def register_view(request):
-#     return render(request, 'auth/login.html')
+def register_view(request):
+    if request.method == "POST":
+        username = request.POST.get('username') or None
+        email = request.POST.get('email') or None
+        password = request.POST.get('password') or None
+        # Django Forms
+        # user_exists = User.objects.filter(username=username).exists()
+        # email_exists = User.objects.filter(email=email).exists()
+        try:
+            User.objects.create_user(
+                username=username, email=email, password=password)
+            print("User created!")
+        except Exception as e:
+            print(e)
+
+    return render(request, 'auth/register.html')
--- src/home/urls.py
+++ src/home/urls.py
@@ -7,6 +7,7 @@ from auth import views as auth_views
 urlpatterns = [
     path("", home_view),
     path("login/", auth_views.login_view),
+    path("register/", auth_views.register_view),
     path('admin/', admin.site.urls),
     path("about/", about_page),
 
